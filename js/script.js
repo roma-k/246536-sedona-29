c3a444a63d92ec290d11e6a5f316c19c0de50cc4
Roman <intere07@ya.ru>
2020-10-23 22:29:02 +0700

    правки
    This reverts commit 100cdf2650523d001c06c68e346c593bc0112c87.

--- js/script.js
+++
@@ -1,6 +0,0 @@
-let indexPopup = document.querySelector('.index_popup_cont');
-let openFormBtn = document.querySelector('.heading_form_white');
-
-openFormBtn.onclick = function() {
-    indexPopup.classList.toggle('visually-hidden');
-}
