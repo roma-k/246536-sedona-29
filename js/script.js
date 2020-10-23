d38a7e556fe865049f731da8d7d3a58839b292ff
Roman <intere07@ya.ru>
2020-10-23 22:22:58 +0700

    правки

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let indexPopup = document.querySelector('.index_popup_cont');
+let openFormBtn = document.querySelector('.heading_form_white');
+
+openFormBtn.onclick = function() {
+    indexPopup.classList.toggle('visually-hidden');
+}
